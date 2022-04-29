5b72c8a2429468da14f79e673a5946ef27d8288e
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2022-04-29 12:15:25 -0400

    updated schema + car dto

--- CarDealership-Schema.sql
+++ CarDealership-Schema.sql
@@ -4,10 +4,8 @@ create database CarDealership;
 use CarDealership;
 
 create table car (
-	id int not null,
+	id int not null AUTO_INCREMENT,
 	year int not null, 
-    make varchar(15) not null,
-    model varchar(15) not null,
     type varchar(4) not null,
     bodyStyle varchar(10) not null, 
     interior varchar(15) not null,
@@ -24,8 +22,34 @@ create table car (
 		primary key(id)
 );
 
+create table make (
+	id int not null AUTO_INCREMENT,
+	name varchar(15) not null,
+    email varchar(15) not null,
+    dateAdded date not null,
+    carId int,
+    constraint pk_make 
+		primary key(id),
+	constraint fk_make
+		foreign key(carId)
+        references car(id)
+);
+
+create table model(
+	id int not null AUTO_INCREMENT, 
+    name varchar(15) not null,
+    email varchar(15) not null, 
+    dateAdded date not null,
+    makeId int not null,
+    constraint pk_model
+		primary key(id),
+	constraint fk_modelMake
+		foreign key(makeId)
+        references make(id)
+);
+
 create table user (
-	id int not null,
+	id int not null AUTO_INCREMENT,
     role varchar(20) not null,
     username varchar(20) not null,
     pw varchar(255) not null,
@@ -34,14 +58,14 @@ create table user (
 );
 
 create table customer (
-	id int not null, 
+	id int not null AUTO_INCREMENT, 
     message varchar(255) not null,
     constraint pk_user
 		primary key(id)
 );
 
 create table person (
-	id int not null,
+	id int not null AUTO_INCREMENT,
     firstName varchar(20) not null,
     lastName varchar(20) not null,
     email varchar(20),
@@ -60,7 +84,7 @@ create table person (
 );
 
 create table sales (
-	id int not null, 
+	id int not null AUTO_INCREMENT, 
     purchasePrice decimal(7,2) not null,
     purchaseType varchar(20) not null,
     carId int not null,
@@ -77,14 +101,16 @@ create table sales (
 );
 
 create table specials(
-	id int not null,
+	id int not null AUTO_INCREMENT,
     title varchar(255) not null, 
-    description varchar(255) not null
+    description varchar(255) not null,
+    constraint pk_specials
+		primary key(id)
 );
 
-insert into car(id, year, make, model, type, bodyStyle, interior, color, mileage, transmission, vin, msrp, salesPrice, available) 
+insert into car(id, year, type, bodyStyle, interior, color, mileage, transmission, vin, msrp, salesPrice, available) 
 values 
-(1, 2022, 'toyota', 'camry', 'new', 'sedan', 'dark gray', 'blue', 23, 'auto', 'xbtznda8qbt7bg1p5', 25395.00, 23000.00, 1),
-(2, 2022, 'ford', 'bronco', 'new', 'suv', 'black', 'navy', 67, 'auto', '7fpdj8e8bz201y9jx', 30800.00, 30000.00, 1),
-(3, 2019, 'honda', 'pilot', 'used', 'suv', 'black', 'silver', 23000, 'auto', '6kezb8mzpq7h9jhpd', 18000.00, 17000.00, 1);
+(1, 2022, 'new', 'sedan', 'dark gray', 'blue', 23, 'auto', 'xbtznda8qbt7bg1p5', 25395.00, 23000.00, 1),
+(2, 2022, 'new', 'suv', 'black', 'navy', 67, 'auto', '7fpdj8e8bz201y9jx', 30800.00, 30000.00, 1),
+(3, 2019, 'used', 'suv', 'black', 'silver', 23000, 'auto', '6kezb8mzpq7h9jhpd', 18000.00, 17000.00, 1);
 
--- src/main/java/com/sg/CarDealership/dto/Car.java
+++ src/main/java/com/sg/CarDealership/dto/Car.java
@@ -4,12 +4,227 @@
  */
 package com.sg.CarDealership.dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author chica
  */
 public class Car {
-    private int id, year, 
+    private int id, year, mileage;
+    private String make, model, type, bodyStyle, interior, color, transmission, vin, description, picture;
+    private double msrp, salesPrice;
+    private boolean available;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + this.id;
+        hash = 79 * hash + this.year;
+        hash = 79 * hash + this.mileage;
+        hash = 79 * hash + Objects.hashCode(this.make);
+        hash = 79 * hash + Objects.hashCode(this.model);
+        hash = 79 * hash + Objects.hashCode(this.type);
+        hash = 79 * hash + Objects.hashCode(this.bodyStyle);
+        hash = 79 * hash + Objects.hashCode(this.interior);
+        hash = 79 * hash + Objects.hashCode(this.color);
+        hash = 79 * hash + Objects.hashCode(this.transmission);
+        hash = 79 * hash + Objects.hashCode(this.vin);
+        hash = 79 * hash + Objects.hashCode(this.description);
+        hash = 79 * hash + Objects.hashCode(this.picture);
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.msrp) ^ (Double.doubleToLongBits(this.msrp) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.salesPrice) ^ (Double.doubleToLongBits(this.salesPrice) >>> 32));
+        hash = 79 * hash + (this.available ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Car other = (Car) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.year != other.year) {
+            return false;
+        }
+        if (this.mileage != other.mileage) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.msrp) != Double.doubleToLongBits(other.msrp)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.salesPrice) != Double.doubleToLongBits(other.salesPrice)) {
+            return false;
+        }
+        if (this.available != other.available) {
+            return false;
+        }
+        if (!Objects.equals(this.make, other.make)) {
+            return false;
+        }
+        if (!Objects.equals(this.model, other.model)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.bodyStyle, other.bodyStyle)) {
+            return false;
+        }
+        if (!Objects.equals(this.interior, other.interior)) {
+            return false;
+        }
+        if (!Objects.equals(this.color, other.color)) {
+            return false;
+        }
+        if (!Objects.equals(this.transmission, other.transmission)) {
+            return false;
+        }
+        if (!Objects.equals(this.vin, other.vin)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return Objects.equals(this.picture, other.picture);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBodyStyle() {
+        return bodyStyle;
+    }
+
+    public void setBodyStyle(String bodyStyle) {
+        this.bodyStyle = bodyStyle;
+    }
+
+    public String getInterior() {
+        return interior;
+    }
+
+    public void setInterior(String interior) {
+        this.interior = interior;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public double getMsrp() {
+        return msrp;
+    }
+
+    public void setMsrp(double msrp) {
+        this.msrp = msrp;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
     
     
 }
